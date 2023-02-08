1dbcb8f949cf6defe4d4848fa9dc8de22179dcaa
Zifan Jiang <j22melody@gmail.com>
2023-02-08 10:47:42 +0100

    allow segmentation outputs probs

--- pose_to_segments/bin.py
+++ pose_to_segments/bin.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 
+import numpy as np
 import pympi
 import torch
 from pose_format import Pose
@@ -27,9 +28,10 @@ def load_pose(pose_path):
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', required=True, type=str, help='path to input pose file')
-    parser.add_argument('-o', required=True, type=str, help='path to output elan file')
-    parser.add_argument('--video', default=None, required=False, type=str, help='path to video file')
+    parser.add_argument('-i', '--input_path', required=True, type=str, help='path to input pose file')
+    parser.add_argument('-o', '--output_path', required=True, type=str, help='path to output file')
+    parser.add_argument('-f', '--output_format', default='elan', required=False, type=str, help='format of output file')
+    parser.add_argument('-v', '--video', default=None, required=False, type=str, help='path to video file')
 
     return parser.parse_args()
 
@@ -38,7 +40,7 @@ def main():
     args = get_args()
 
     print('Loading pose ...')
-    pose = load_pose(args.i)
+    pose = load_pose(args.input_path)
 
     print('Loading model ...')
     install_dir = os.path.dirname(os.path.abspath(__file__))
@@ -51,29 +53,34 @@ def main():
         pose_data = torch_body.data.tensor[:, 0, :, :].unsqueeze(0)
         probs = model(pose_data)
 
-    sign_segments = probs_to_segments(probs["sign"])
-    sentence_segments = probs_to_segments(probs["sentence"], .5, .5)
+    if args.output_format == 'probs':
+        print('Saving probs as numpy array ...')
+        np.save(args.output_path, probs)
 
-    print('Building ELAN file ...')
-    tiers = {
-        "SIGN": sign_segments,
-        "SENTENCE": sentence_segments,
-    }
+    if args.output_format == 'elan':
+        sign_segments = probs_to_segments(probs["sign"])
+        sentence_segments = probs_to_segments(probs["sentence"], .5, .5)
 
-    fps = pose.body.fps
+        print('Building ELAN file ...')
+        tiers = {
+            "SIGN": sign_segments,
+            "SENTENCE": sentence_segments,
+        }
 
-    eaf = pympi.Elan.Eaf(author="sign-langauge-processing/transcription")
-    if args.video is not None:
-        mimetype = None  # pympi is not familiar with mp4 files
-        if args.video.endswith(".mp4"):
-            mimetype = "video/mp4"
-        eaf.add_linked_file(args.video, mimetype=mimetype)
-    eaf.add_linked_file(args.i, mimetype="application/pose")
+        fps = pose.body.fps
 
-    for tier_id, segments in tiers.items():
-        eaf.add_tier(tier_id)
-        for segment in segments:
-            eaf.add_annotation(tier_id, segment["start"] * fps, segment["end"] * fps)
+        eaf = pympi.Elan.Eaf(author="sign-langauge-processing/transcription")
+        if args.video is not None:
+            mimetype = None  # pympi is not familiar with mp4 files
+            if args.video.endswith(".mp4"):
+                mimetype = "video/mp4"
+            eaf.add_linked_file(args.video, mimetype=mimetype)
+        eaf.add_linked_file(args.input_path, mimetype="application/pose")
 
-    print('Saving to disk ...')
-    eaf.to_file(args.o)
+        for tier_id, segments in tiers.items():
+            eaf.add_tier(tier_id)
+            for segment in segments:
+                eaf.add_annotation(tier_id, segment["start"] * fps, segment["end"] * fps)
+
+        print('Saving to disk ...')
+        eaf.to_file(args.output_path)
